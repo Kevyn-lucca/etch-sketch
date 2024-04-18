c6c5949522fbc701eacdc8eb90443decc6112f01
kevyn <kevynalternativo@gmail.com>
2024-04-18 11:47:03 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
+//content pulled from the page
 const container = document.querySelector(".container");
 const btn = document.querySelector("button");
 
-btn.addEventListener("click", () => {
-    clearDivs();
-    generateDivs();
-});
 
-function clearDivs() {
+//this function makes a trail that follows the mouse around
+function MousePainter(div){
+    div.addEventListener("mousemove", (e) => {
+        const mouseX = e.offsetX;
+        const mouseY = e.offsetY;
+        const dot = document.createElement("div");
+        dot.classList.add("dot");
+        dot.style.left = `${mouseX}px`;
+        dot.style.top = `${mouseY}px`;
+        div.appendChild(dot);
+        
+    });
+    }
+
+//this function generates random rgb numbers and applys them on the generated divs
+    function ColorDiv(div){
+        div.addEventListener("mousemove", (e) => {
+        let num1 = Math.random() * 255
+        let num2 = Math.random() * 255
+        let num3 = Math.random() * 255
+        div.style.backgroundColor = `rgb(${num1},${num2}, ${num3})`;
+    });
+    }
+
+
+ //this buttom clears the canvas and calls generatDivs   
+btn.addEventListener("click", () => {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
-}
+    generateDivs();
+});
 
+
+//this function generate new divs
 function generateDivs() {
     let divSize = prompt("Enter the number of divs (up to 100): ");
     divSize = parseInt(divSize);
@@ -19,22 +45,15 @@ function generateDivs() {
         alert("Please enter a valid number between 1 and 100.");
         return;
     }
-
+    //this loop generates the required number of divs and adds the previuws functions to each
     for (let i = 0; i < divSize; i++) {
-        let div = document.createElement("div"); // Create a new div each time
+        let div = document.createElement("div");
         div.classList.add("div-sketch");
         container.appendChild(div);
 
-        // Add mousemove event listener to each div
-        div.addEventListener("mousemove", (e) => {
-            const mouseX = e.offsetX;
-            const mouseY = e.offsetY;
-            const dot = document.createElement("div");
-            dot.classList.add("dot");
-            dot.style.left = `${mouseX}px`;
-            dot.style.top = `${mouseY}px`;
-            div.appendChild(dot); // Adiciona o ponto à div atual
-            
-        });
+        MousePainter(div)
+        ColorDiv(div)
     }
 }
+
+// kisses from brasil
